c50754339613eced9a99432313dfaf1f9b97bbb3
Jason Spidle <jason.spidle@gmail.com>
2015-08-28 15:17:12 -0500

    Add cute route.

---
+++ routes/cute.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+var path = require('path');
+
+router.get('/:id?', function(req, res, next){
+
+    var id = req.params.id;
+
+    var file = path.join(__dirname, '../models/cute.json');
+
+    fs.readFile(file, 'utf8', function(err, data){
+
+        if(err){
+            next(err);
+        } else{
+            var obj = JSON.parse(data);
+            var cuteImg = obj;
+
+            obj.forEach(function(elem){
+                if(elem.id == id){
+                    cuteImg = elem;
+                }
+            });
+
+            res.json(cuteImg);
+        }
+    })
+});
+
+module.exports = router;
+
