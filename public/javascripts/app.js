865065a89d9e0e5143670cf75cc5d76160cc2a47
Brendan Telzrow <telzrowb@gmail.com>
2015-08-28 16:31:16 -0500

    added generate new deck functionality, swap card buttons

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,29 +1,60 @@
+var $cuteDinos = $('#cuteDinos');
+
+
 $(document).ready(function(){
+    AJAXcall();
 
-    $.ajax({
-        type: 'GET',
-        dataType: 'json',
-        url: '/cute'})
+    $cuteDinos.on('click', '#getDeck', function(){
+        $cuteDinos.empty();
+        AJAXcall();
+    });
 
-        .done(function(data){
-            var shuffled = shuffle(data);
-            var cute5 = shuffled.splice(0, 5);
+    $cuteDinos.on('click', '#changeDino', function(){
+        var dinos = ['dino1', 'dino2', 'dino3', 'dino4', 'dino5', 'dino6'];
 
-            var $cuteDinos = $('#cuteDinos');
+        $('div').each(function(index){
+            console.log($(this).data('id'));
+        })
 
-            cute5.forEach(function(elem){
-                var $div = $('<div>').html('<img src=\"' + elem.imgLink + '\" alt=\"' + elem.id + '\">');
-                    $cuteDinos.append($div);
+
+    });
+
+    function AJAXcall(){
+        $.ajax({
+            type: 'GET',
+            dataType: 'json',
+            url: '/cute'})
+
+            .done(function(data){
+                var $buttonGenerate = $('<button>').text('Change Deck').attr('id','getDeck');
+                $cuteDinos.prepend($buttonGenerate);
+                displayNewDeck(data);
+
             })
-        })
 
-        .fail(function(jqXHR, textStatus, errorThrown){
-            console.log('There was an error');
-        })
-        .always(function(){
-            console.log('AJAX complete');
-        })
+            .fail(function(jqXHR, textStatus, errorThrown){
+                console.log('There was an error');
+            })
+            .always(function(){
+                console.log('AJAX complete');
+            })
+
+    }
+
 });
+
+function displayNewDeck(data){
+    var shuffled = shuffle(data);
+    var cute5 = shuffled.splice(0, 5);
+
+    cute5.forEach(function(elem){
+        var $buttonChange = $('<button>').text('Swap Dino').attr('id','changeDino').attr('data-id', elem.id);
+        var $div = $('<div>').attr('data-id',elem.id).html('<img src=\"' + elem.imgLink + '\" alt=\"' + elem.id + '\">').append($buttonChange);
+        $cuteDinos.append($div);
+    });
+    return cute5;
+}
+
 
 function shuffle(array) {
     var m = array.length, t, i;
